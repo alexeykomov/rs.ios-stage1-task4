85f56a82e389ab0cd145ab823a6fb4d027184e4e
alexeykomov <alexeykofficial@gmail.com>
2020-04-12 16:46:02 +0300

    [WIP] feat: first solution, not all tests are passed

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,7 +2,43 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    // good luck
-    return @[@0];
+    int initialNumber = [(NSNumber*) number intValue];
+    int maximalClosestNumber = initialNumber - 1;
+    int leftSpace = powl(initialNumber, 2);
+    NSMutableArray *res = [[NSMutableArray alloc] init];
+    while (maximalClosestNumber > 0) {
+        int leftSpaceAfterSubtraction = leftSpace - pow(maximalClosestNumber, 2);
+        if (leftSpaceAfterSubtraction >= 0) {
+            [res addObject:@(maximalClosestNumber)];
+            leftSpace = leftSpaceAfterSubtraction;
+        }
+        maximalClosestNumber--;
+        
+    }
+    NSLog(@"Result: %@", res);
+    NSLog(@"Left space: %d", leftSpace);
+    return [[res reverseObjectEnumerator] allObjects];
 }
+
+- (NSArray <NSNumber*>*)decomposeNumberRec:(NSNumber*)number
+                                 leftSpace:(int) leftSpace{
+    int initialNumber = [(NSNumber*) number intValue];
+    int maximalClosestNumber = initialNumber - 1;
+    NSMutableArray *res = [[NSMutableArray alloc] init];
+    while (maximalClosestNumber > 0) {
+        int leftSpaceAfterSubtraction = leftSpace - pow(maximalClosestNumber, 2);
+        if (leftSpaceAfterSubtraction >= 0) {
+            [self decomposeNumberRec:maximalClosestNumber leftSpace:leftSpaceAfterSubtraction];
+            
+            [res addObject:@(maximalClosestNumber)];
+            leftSpace = leftSpaceAfterSubtraction;
+        }
+        maximalClosestNumber--;
+        
+    }
+    NSLog(@"Result: %@", res);
+    NSLog(@"Left space: %d", leftSpace);
+    return [[res reverseObjectEnumerator] allObjects];
+}
+
 @end
